0dd31e4383043cdd9a6be4cba3cd9a8911103d55
Mahdi-Marv <mahdihajialilue@gmail.com>
2024-05-03 11:36:11 +0330

    fix bug

--- train.py
+++ train.py
@@ -74,7 +74,8 @@ for epoch in range(start_epoch, P.epochs + 1):
             "--normal_labels", str(P.normal_labels),
             "--noise_scale",str(0.0),
             "--noist_probability", str(0.0),
-            '--activation_function', str(P.activation_function)
+            '--activation_function', str(P.activation_function),
+            '--brats', str(P.brats)
         ]
 
         result = subprocess.run(["python", "eval.py"] + arguments_to_pass, capture_output=True, text=True)
