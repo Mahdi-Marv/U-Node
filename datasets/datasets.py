d4cca48527c948d3040a3fab8eb13b68e8da8343
Arad Maleki <arad.m.t02@gmail.com>
2024-05-04 15:53:20 +0330

    debug

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -838,7 +838,7 @@ def get_dataset(P, dataset, test_only=False, image_size=(32, 32, 3), download=Fa
         n_classes = 2
         train_dataset = []
         test_dataset = []
-        root = "/kaggle/input/mvtec-ad"
+        root = "./mvtec_anomaly_detection"
         train_transform = transforms.Compose([
                 transforms.Resize((256, 256)),
                 transforms.CenterCrop((image_size[0], image_size[1])),
