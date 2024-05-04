dad73e5811215badbe4a15c92ef2d27f90453db2
Arad Maleki <arad.m.t02@gmail.com>
2024-05-04 11:56:07 +0330

    debug

--- evals/ood_pre.py
+++ evals/ood_pre.py
@@ -120,8 +120,8 @@ def get_scores(P, feats_dict, ood_score):
         score = 0
         for shi in range(P.K_shift):
             # score += (f_sim[shi] * P.axis[shi]).sum(dim=1).max().item() * P.weight_sim[shi]
-            # score += f_shi[shi][:, shi].item() * P.weight_shi[shi]
-            score += f_shi[shi][:, shi].item() * 1
+            score += f_shi[shi][:, shi].item() * P.weight_shi[shi]
+            # score += f_shi[shi][:, shi].item() * 1
             break
         score = score / P.K_shift
         scores.append(score)
