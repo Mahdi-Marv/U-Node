0e14de9ce9a7bf09bb47202032c4fa0e6f9f606a
Arad Maleki <arad.m.t02@gmail.com>
2024-05-04 13:54:43 +0330

    debug

--- datasets/custom_datasets.py
+++ datasets/custom_datasets.py
@@ -113,7 +113,7 @@ class MVTecDataset(Dataset):
         if index == 0:
             print('train:', self.train)
             to_ten = torchvision.transforms.ToTensor()
-            print(to_ten(image)[0][0][0])
+            print(to_ten(image)[0][0])
         if self.transform is not None:
             image = self.transform(image)
         if os.path.dirname(image_file).endswith("good"):
