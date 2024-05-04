2b6b61b95a2de08281d5ecec5361ef67c6eec791
Arad Maleki <arad.m.t02@gmail.com>
2024-05-04 16:46:18 +0330

    debug

--- evals/ood_pre.py
+++ evals/ood_pre.py
@@ -223,6 +223,7 @@ def _get_features(P, model, loader, interp=False, imagenet=False, simclr_aug=Non
         # add features in full dataset
         for layer in layers:
             feats_all[layer] += [feats_batch[layer]]
+        print(f'{i - 1}: {feats_all["shift"][-1]}')
 
     # concatenate features in full dataset
     for key, val in feats_all.items():
